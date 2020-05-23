307ce174f93bdfdcb949e1f0a7f16f6ac48cd91d
Eylen <eylenkim@gmail.com>
2020-05-22 19:44:37 -0700

    final css touches

--- index.js
+++ index.js
@@ -4,15 +4,51 @@ const collection = document.querySelector(".collection");
 
 //add breed to collection function
 async function addDog(breedType) {
+	var breedName, result, resultJson;
+
 	if (breedType === "random") {
 		url = "https://dog.ceo/api/breeds/image/random";
+		result = await fetch(url);
+		resultJson = await result.json();
+		data = resultJson.message;
+
+		//omit sub-breed and only display main breed
+		function indexSymbol() {
+			//if there is a sub breed, end of the index is - before it starts
+			if (data.slice(0, data.indexOf("/", 30)).includes("-")) {
+				return "-";
+				//else, if there isn't a sub breed AND there is a - after breedName/, end of index is the fifth /
+			} else if (
+				!(
+					data.slice(0, data.indexOf("/", 30)).includes("-") &&
+					data.includes("-", data.indexOf("/", 31))
+				)
+			) {
+				return "/";
+			} else {
+				return "/";
+			}
+		}
+
+		var lastIndex = data.indexOf(indexSymbol(), 30);
+		var slicedName = data.slice(30, lastIndex);
+		breedName = slicedName.toUpperCase();
+
+		//remove duplicate random. If duplicate, run random function again
+		let pChild = collection.querySelectorAll("p");
+		for (let i = 0; i < pChild.length; i++) {
+			if (pChild[i].textContent === breedName) {
+				return addDog("random");
+				breedName.remove();
+			}
+		}
 	} else {
 		url = `https://dog.ceo/api/breed/${breedType}/images/random`;
+		breedName = breedType.toUpperCase();
+		result = await fetch(url);
+		resultJson = await result.json();
+		data = resultJson.message;
 	}
-
-	const result = await fetch(url);
-	const resultJson = await result.json();
-	const data = resultJson.message;
 
 	const img = document.createElement("img");
 	const p = document.createElement("p");
@@ -20,21 +56,23 @@ async function addDog(breedType) {
 	const deleteBtn = document.createElement("button");
 
 	img.src = data;
-	img.alt = `Picture of ${breedType}`;
+	img.alt = `Picture of ${breedName}`;
 	div.className = "dog-entry";
 	deleteBtn.className = "delete-btn";
 
-	p.textContent = breedType;
+	p.textContent = breedName.toUpperCase();
+	deleteBtn.textContent = '⊗';
+
 	collection.appendChild(div);
 	div.appendChild(p);
-	div.appendChild(img);
 	div.appendChild(deleteBtn);
+	div.appendChild(img);
 
 	//remove breed
 	let deleteLink = collection.querySelectorAll(".delete-btn");
 	for (var i = 0; i < deleteLink.length; i++) {
 		deleteLink[i].addEventListener("click", function() {
-			alert("Remove dog function worked!");
+			this.parentElement.remove();
 		});
 	}
 }
@@ -61,7 +99,7 @@ async function selectDog() {
 	dropdown.selectedIndex = 0;
 	for (let i = 0; i < data.length; i++) {
 		option = document.createElement("option");
-		option.text = data[i];
+		option.text = data[i].toUpperCase();
 		option.value = data[i];
 		dropdown.add(option);
 	}
@@ -70,8 +108,9 @@ async function selectDog() {
 		//duplicate breed alert
 		let pChild = collection.querySelectorAll("p");
 		for (let i = 0; i < pChild.length; i++) {
-			if (pChild[i].textContent === this.value) {
-				alert("You have already added this breed");
+			if (pChild[i].textContent === this.value.toUpperCase()) {
+				alert("This breed is already in your collection.");
+				this.value.remove();
 			}
 		}
 		//add breed to collection
